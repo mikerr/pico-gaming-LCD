8c94378684166f496456cdc1e1de135d6927cfc7
Mike Redrobe <mike@redrobe.net>
2024-05-23 11:56:14 +0100

    Create bmp2screen.py

---
+++ bmp2screen.py
@@ -0,0 +1,208 @@
+# load bmp direct to screen
+# 24bit BMP, uncompressed
+
+from machine import Pin,SPI,PWM
+import framebuf,gc
+import time
+
+BL = 13
+DC = 8
+RST = 12
+MOSI = 11
+SCK = 10
+CS = 9
+
+class LCD_1inch3(framebuf.FrameBuffer):
+    def __init__(self):
+        self.width = 240
+        self.height = 240
+        
+        self.cs = Pin(CS,Pin.OUT)
+        self.rst = Pin(RST,Pin.OUT)
+        
+        self.cs(1)
+        self.spi = SPI(1)
+        self.spi = SPI(1,1000_000)
+        self.spi = SPI(1,100000_000,polarity=0, phase=0,sck=Pin(SCK),mosi=Pin(MOSI),miso=None)
+        self.dc = Pin(DC,Pin.OUT)
+        self.dc(1)
+        self.buffer = bytearray(self.height * self.width * 2)
+        super().__init__(self.buffer, self.width, self.height, framebuf.RGB565)
+        self.init_display()
+        
+        self.red   =   0x07E0
+        self.green =   0x001f
+        self.blue  =   0xf800
+        self.white =   0xffff
+        
+    def write_cmd(self, cmd):
+        self.cs(1)
+        self.dc(0)
+        self.cs(0)
+        self.spi.write(bytearray([cmd]))
+        self.cs(1)
+
+    def write_data(self, buf):
+        self.cs(1)
+        self.dc(1)
+        self.cs(0)
+        self.spi.write(bytearray([buf]))
+        self.cs(1)
+
+    def init_display(self):
+        """Initialize dispaly"""  
+        self.rst(1)
+        self.rst(0)
+        self.rst(1)
+        
+        self.write_cmd(0x36)
+        self.write_data(0x70)
+
+        self.write_cmd(0x3A) 
+        self.write_data(0x05)
+
+        self.write_cmd(0xB2)
+        self.write_data(0x0C)
+        self.write_data(0x0C)
+        self.write_data(0x00)
+        self.write_data(0x33)
+        self.write_data(0x33)
+
+        self.write_cmd(0xB7)
+        self.write_data(0x35) 
+
+        self.write_cmd(0xBB)
+        self.write_data(0x19)
+
+        self.write_cmd(0xC0)
+        self.write_data(0x2C)
+
+        self.write_cmd(0xC2)
+        self.write_data(0x01)
+
+        self.write_cmd(0xC3)
+        self.write_data(0x12)   
+
+        self.write_cmd(0xC4)
+        self.write_data(0x20)
+
+        self.write_cmd(0xC6)
+        self.write_data(0x0F) 
+
+        self.write_cmd(0xD0)
+        self.write_data(0xA4)
+        self.write_data(0xA1)
+
+        self.write_cmd(0xE0)
+        self.write_data(0xD0)
+        self.write_data(0x04)
+        self.write_data(0x0D)
+        self.write_data(0x11)
+        self.write_data(0x13)
+        self.write_data(0x2B)
+        self.write_data(0x3F)
+        self.write_data(0x54)
+        self.write_data(0x4C)
+        self.write_data(0x18)
+        self.write_data(0x0D)
+        self.write_data(0x0B)
+        self.write_data(0x1F)
+        self.write_data(0x23)
+
+        self.write_cmd(0xE1)
+        self.write_data(0xD0)
+        self.write_data(0x04)
+        self.write_data(0x0C)
+        self.write_data(0x11)
+        self.write_data(0x13)
+        self.write_data(0x2C)
+        self.write_data(0x3F)
+        self.write_data(0x44)
+        self.write_data(0x51)
+        self.write_data(0x2F)
+        self.write_data(0x1F)
+        self.write_data(0x1F)
+        self.write_data(0x20)
+        self.write_data(0x23)
+        
+        self.write_cmd(0x21)
+
+        self.write_cmd(0x11)
+
+        self.write_cmd(0x29)
+
+    def show(self):
+        self.write_cmd(0x2A)
+        self.write_data(0x00)
+        self.write_data(0x00)
+        self.write_data(0x00)
+        self.write_data(0xef)
+        
+        self.write_cmd(0x2B)
+        self.write_data(0x00)
+        self.write_data(0x00)
+        self.write_data(0x00)
+        self.write_data(0xEF)
+        
+        self.write_cmd(0x2C)
+        
+        self.cs(1)
+        self.dc(1)
+        self.cs(0)
+        self.spi.write(self.buffer)
+        self.cs(1)
+
+def readbmp(fb,filename):
+        def lebytes_to_int(bytes):
+            n = 0x00
+            while len(bytes) > 0:
+                n <<= 8
+                n |= bytes.pop()
+            return int(n)
+
+        f = open(filename, 'rb') 
+        img_bytes = list(bytearray(f.read(26))) # just read header
+        start_pos = lebytes_to_int(img_bytes[10:14])
+
+        width = lebytes_to_int(img_bytes[18:22])
+        height = lebytes_to_int(img_bytes[22:26])
+        
+        seektostart = f.read(start_pos - 26)
+                             
+        #gc.collect()
+        #buffer = bytearray(height * width *2)
+        #sprite1 = framebuf.FrameBuffer(buffer, width, height, framebuf.RGB565)
+        for x in range(height):
+            colrow = list(bytearray(f.read(3 * width)))
+            #if ( x % 30 == 0) : LCD.show()
+            for y in range(width):
+                b,g,r = colrow[y*3:y*3+3]
+                # RGB565
+                rgb = ((r >> 3)  << 11) | ((g >>2) << 5) | (b >> 3 )
+                
+                # swap needed for ILI9341 screen
+                swapL = rgb >> 8
+                swapH = (rgb & 0x00FF) << 8
+                col = swapL | swapH
+                
+                fb.pixel(y,height - x,col)
+                
+        f.close()
+        return 
+    
+if __name__=='__main__':
+    pwm = PWM(Pin(BL))
+    pwm.freq(1000)
+    pwm.duty_u16(32768)#max 65535
+
+    LCD = LCD_1inch3()
+     
+    LCD.fill(0)
+    #LCD.show()
+    while True:
+        readbmp(LCD,"lena.bmp")
+        LCD.show()
+        readbmp(LCD,"mandrill.bmp")
+        LCD.show()
+        readbmp(LCD,"peppers.bmp")
+        LCD.show()
